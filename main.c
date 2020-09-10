47660ee25bb14d84dbb25bce255b9255123c8001
Shakeb Siddiqui <sms8508@psu.edu>
2020-09-10 17:43:19 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+// Author: Shakeb Siddiqui sms8508@psu.edu
+// Collaborator:
+// Collaborator:
+// Collaborator:
+// Section: 
+// Breakout: 
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
